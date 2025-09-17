7ba459b2020c0c52deaf9b2ba26518f8cc9e96a5
Onyilimba M Tochukwu <tochimclaren@gmail.com>
2025-09-17 12:27:08 +0100

    Product loading position update

--- src/pages/cms/product/Products.tsx
+++ src/pages/cms/product/Products.tsx
@@ -101,11 +101,11 @@ function Products() {
     };
 
     if (loading) {
-        return <div className="p-4 w-full">Loading data...</div>;
+        return <div className="p-4">Loading data...</div>;
     }
 
     if (error) {
-        return <div className="p-4 text-red-600 w-full">{error}</div>;
+        return <div className="p-4 text-red-600">{error}</div>;
     }
 
     return (
